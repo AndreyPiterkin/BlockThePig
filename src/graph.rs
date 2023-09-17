be475c9d0e0bbc77c2679a03a21db5663bdc05c7
Andrey Piterkin <andrey.piterkin@gmail.com>
2023-09-17 11:39:40 -0400

    feat: added setters for neighbor initialization

--- src/graph.rs
+++ src/graph.rs
@@ -1,11 +1,10 @@
 use std::cell::Cell;
-
-pub struct Board {
-    board: Vec<Vec<Tile>>,
+pub struct Board<'a> {
+    board: Vec<Vec<Tile<'a>>>,
 }
 
-pub enum Tile {
-    Free(FreeTile),
+pub enum Tile<'a> {
+    Free(FreeTile<'a>),
     Edge,
     Blocked,
 }
@@ -15,26 +14,57 @@ pub enum Tile {
 * It has a collection of neighbors, guaranteed to be sorted 
 * from top-left, moving clockwise and ending at right.
 */
-pub struct FreeTile {
-    neighbors: Vec<Cell<Tile>>
+pub struct FreeTile<'a> {
+    top_left: Cell<&'a Tile<'a>>,
+    top_right: Cell<&'a Tile<'a>>,
+    right: Cell<&'a Tile<'a>>,
+    bot_right: Cell<&'a Tile<'a>>,
+    bot_left: Cell<&'a Tile<'a>>,
+    left: Cell<&'a Tile<'a>>
 }
 
-impl FreeTile {
-    fn new() -> FreeTile {
+impl<'a> FreeTile<'a> {
+    fn new(initial_neighbor: &'a Tile<'a>) -> FreeTile<'a> {
         FreeTile {
-            neighbors: vec![],
+            top_left: Cell::new(initial_neighbor),
+            top_right: Cell::new(initial_neighbor),
+            right: Cell::new(initial_neighbor),
+            bot_right: Cell::new(initial_neighbor),
+            bot_left: Cell::new(initial_neighbor),
+            left: Cell::new(initial_neighbor)
         }
     }
 
-    fn add_neighbor(&mut self, t: &Tile) {
-        self.neighbors.push(t);
+    fn set_top_left(&self, tl: &'a Tile<'a>) -> () {
+        self.top_left.set(tl);
+    }
+
+    fn set_top_right(&self, tl: &'a Tile<'a>) -> () {
+        self.top_right.set(tl);
+    }
+    
+    fn set_right(&self, tl: &'a Tile<'a>) -> () {
+        self.right.set(tl);
+    }
+
+    fn set_bot_right(&self, tl: &'a Tile<'a>) -> () {
+        self.bot_right.set(tl);
+    }
+
+    fn set_bot_left(&self, tl: &'a Tile<'a>) -> () {
+        self.bot_left.set(tl);
+    }
+
+    fn set_left(&self, tl: &'a Tile<'a>) -> () {
+        self.left.set(tl);
     }
 }
 
-impl Board {
-    pub fn new(row_count: usize, col_count: usize) -> Board {
+impl<'a> Board<'a> {
+    pub fn new(row_count: usize, col_count: usize) -> Board<'a> {
+        let initializer_tile : &'a Tile<'a> = &Tile::Blocked;
         let b = Board {
-            board: (0..row_count).map(|r| (0..col_count).map(|c| Tile::Free(FreeTile::new())).collect()).collect()
+            board: (0..row_count).map(|r| (0..col_count).map(|c| Tile::Free(FreeTile::new(initializer_tile))).collect()).collect()
         };
         
         return b;
