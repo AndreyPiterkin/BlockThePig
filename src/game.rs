531756840fe74c16d96c534c72a8004d03f4c087
Andrey Piterkin <andrey.piterkin@gmail.com>
2023-10-09 22:57:05 -0400

    feat: modified game implementation to match new board public API

--- src/game.rs
+++ src/game.rs
@@ -1,4 +1,5 @@
 use crate::board::{Board, Tile};
+use crate::maps::ClassicMap;
 
 pub struct GameInstance {
     board: Board,
@@ -33,18 +34,20 @@ impl Pig {
 impl GameInstance {
     pub fn classic_game() -> GameInstance {
         GameInstance {
-            board: Board::new(11, 5),
+            board: Board::new(ClassicMap::new(11, 5)),
             pig: Pig::standard(),
         }
     }
 
     pub fn get_dimensions(&self) -> (usize, usize) {
-        self.board.get_dimensions()
+        let ((br, bc), (er, ec)) = self.board.get_dimensions();
+        return (er - br, ec - bc)
     }
 
-    // TODO: Replace tile-at with some rendering logic in here to avoid for loop in main
     pub fn tile_at(&self, (r,c): (usize, usize)) -> Tile {
-        self.board.get_tile((r, c)).clone().into_inner()
+        let ((br, bc), _) = self.board.get_dimensions();
+        let (ir, ic) = (r + br, c + bc);
+        self.board.get_tile((ir, ic)).clone()
     }
 
     pub fn pig_pos(&self) -> (usize, usize) {
